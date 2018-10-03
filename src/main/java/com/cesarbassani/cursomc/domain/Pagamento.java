dcaa1ae558fe95df06edcde3f3efc82ec56ff2b7
cesarbassani <cesarbassani@digix.com.br>
2018-10-03 12:24:19 -0400

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/cesarbassani/cursomc/domain/Pagamento.java
+++ src/main/java/com/cesarbassani/cursomc/domain/Pagamento.java
@@ -1,7 +1,7 @@
 package com.cesarbassani.cursomc.domain;
 
 import com.cesarbassani.cursomc.domain.enuns.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -17,7 +17,7 @@ public abstract class Pagamento implements Serializable {
 
     private Integer estado;
 
-    @JsonBackReference
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name = "pedido_id")
     @MapsId
--- src/main/java/com/cesarbassani/cursomc/domain/Produto.java
+++ src/main/java/com/cesarbassani/cursomc/domain/Produto.java
@@ -1,6 +1,5 @@
 package com.cesarbassani.cursomc.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
@@ -19,7 +18,7 @@ public class Produto implements Serializable {
     private String nome;
     private Double preco;
 
-    @JsonBackReference
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
     private List<Categoria> categorias = new ArrayList<>();
