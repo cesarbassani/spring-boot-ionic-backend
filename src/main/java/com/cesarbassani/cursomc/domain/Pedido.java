5d4db901adcedc0a65243a4af6f75f0247f5e4c1
cesarbassani <cesarbassani@digix.com.br>
2018-10-03 11:52:34 -0400

    ItemPedido e ItemPedidoPK

--- src/main/java/com/cesarbassani/cursomc/domain/Pedido.java
+++ src/main/java/com/cesarbassani/cursomc/domain/Pedido.java
@@ -3,7 +3,9 @@ package com.cesarbassani.cursomc.domain;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Pedido implements Serializable {
@@ -27,6 +29,9 @@ public class Pedido implements Serializable {
     @JoinColumn(name = "endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
 
+    @OneToMany(mappedBy = "id.pedido")
+    private Set<ItemPedido> itens =  new HashSet<>();
+
     public Pedido() {
     }
 
@@ -34,6 +39,14 @@ public class Pedido implements Serializable {
         this.instante = instante;
         this.cliente = cliente;
         this.enderecoDeEntrega = enderecoDeEntrega;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     public Integer getId() {
