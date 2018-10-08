fd44d007c8316e2ef8068239b066b618544783d3
cesarbassani <cesarbassani@digix.com.br>
2018-10-08 10:08:15 -0400

    Criando profile de test

---
+++ src/main/java/com/cesarbassani/cursomc/config/TesteConfig.java
@@ -0,0 +1,24 @@
+package com.cesarbassani.cursomc.config;
+
+import com.cesarbassani.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TesteConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+
+        return true;
+    }
+}
